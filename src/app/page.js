5a3e9133d8d94390ee71f10f1ab3568cd7c3c215
SIFAT HOSSAIN <143956074+sifathossain232@users.noreply.github.com>
2024-07-27 10:54:07 +0600

    Edit import options

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import Navbar from "@/Components/Header/Navbar";
+import Navbar from "../Components/Header/Navbar";
 import About from "../Components/About/About";
 import Banner from "../Components/Banner/Banner.js";
 import CaseStudies from "../Components/CaseStudies/CaseStudies";
 import ContactUs from "../Components/ContactUs/ContactUs";
-import PositiveusFooter from "@/Components/PositiveusFooter/PositiveusFooter";
-import Services from "@/Components/Services/Services";
-import Team from "@/Components/Team/Team";
-import Testimonials from "@/Components/Testimonials/Testimonials";
-import WorkingProcess from "@/Components/WorkingProcess/WorkingProcess";
+import PositiveusFooter from "../Components/PositiveusFooter/PositiveusFooter";
+import Services from "../Components/Services/Services";
+import Team from "../Components/Team/Team";
+import Testimonials from "../Components/Testimonials/Testimonials";
+import WorkingProcess from "../Components/WorkingProcess/WorkingProcess";
 
 export default function Home() {
   return (
